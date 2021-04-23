1331a632817f5700767546c629c4dca0d60f5596
Isabela Blucher <isabelablucher@gmail.com>
2021-04-23 15:28:55 -0300

    Add spaces to arithmetic operations

--- mini_ep1/mini_ep1.py
+++ mini_ep1/mini_ep1.py
@@ -6,14 +6,14 @@ N = 1 << int(input())
 primos = 0
 primos_especiais = 0
 
-crivo = [True] * (N+1)
+crivo = [True] * (N + 1)
 
-for i in range(2, N+1):
+for i in range(2, N + 1):
     if crivo[i]:
         primos += 1
         if i % 4 != 3:
             primos_especiais += 1
-        for j in range(i*2, N+1, i):
+        for j in range(i * 2, N + 1, i):
             crivo[j] = False
 
 print(primos, primos_especiais)
